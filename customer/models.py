ad78e155c4093b01a03eb11dba3455c8069dc42f
rateterin <rateterin@yandex.ru>
2021-08-06 11:36:17 +0300

    Для приложений Customer, Deal созданы необходимые модели: Customer - для хранения информации о клиентах Top5 - кэш "5 клинтов потративших максимальную сумму и камни.." Deal - история сделок ImportLog - лог успешных и не успешных загрузок. Отчасти для проверки актуальности Top5 кэша.

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    username = models.CharField(max_length=48, primary_key=True)
+    spent_money = models.PositiveBigIntegerField(default=0)
+
+
+class Top5(models.Model):
+    username = models.CharField(max_length=48, primary_key=True)
+    spent_money = models.PositiveBigIntegerField(default=0)
+    gems = models.CharField(default='', max_length=512)
--- deal/models.py
+++ deal/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Deal(models.Model):
+    customer = models.ForeignKey(to='customer.Customer', to_field='username', on_delete=models.CASCADE)
+    item = models.CharField(max_length=24, blank=False, null=False)
+    total = models.PositiveBigIntegerField(default=0, blank=False, null=False)
+    quantity = models.PositiveIntegerField(default=0, blank=False, null=False)
+    date = models.DateTimeField(default=timezone.now)
+
+
+class ImportLog(models.Model):
+    file = models.FileField(upload_to='deals')
+    date = models.DateTimeField(auto_now=True)
+    success = models.BooleanField(default=False)
